7451ce63aa337728263e315df4608d620fb04c4d
Bhargav Thota <bhargav.thota@ibm.com>
2025-05-16 13:19:04 +0530

    Added volatile keyword to avoid data inconsistency.

--- LLD/Design-Patterns/creational/singleton/src/Singleton.java
+++ LLD/Design-Patterns/creational/singleton/src/Singleton.java
@@ -1,6 +1,15 @@
+
+/*
+The volatile keyword is a modifier that ensures that an attribute's value is always the same when read from all threads.
+
+Normally the value of an attribute may be written into a thread's local cache and not updated in the main memory for some amount of time.
+In this case, other threads will see a different value for the attribute.
+The volatile keyword makes sure that threads always update the value of an attribute in main memory.
+*/
+
 public class Singleton {
 
-    private static Singleton instance = null;
+    private static volatile Singleton instance = null;
 
     private Singleton() {
         System.out.println("Inside Singleton constructor");
